14fde7064500ec118ffca3fb92b07acb2a15ab79
fuzz <allandiego252@gmail.com>
2022-04-30 18:56:55 -0300

    Adicionando a função de selecionar o item da listView e o floatingBottom

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # cryptomoedas
 
 Aprendendo flutter.
+
+
+# Ciclo de vida do Flutter 
+
+Ao mutar o estado com setState o 
+flutter chama o build novamente. 
+
+Árvore - 
+ - CreateState() -> Cria um estado inicial para o widget. 
+
+ - initState(): Bom momento para consumir API'S
+
+ - didChangeDependencies() -> essa função no cilco de vida verifica se todos os objetos exeternos que estão sendo utilzados já estão instânciados. 
+
+ - STATE -> é o objeto de estado do widget. 
+ - build()  -> Esse método é chamado para construir a WIDGET 
+
+ - Widget -> depois que a Widget foi construida agora há um objeto widget. 
+ - setState() -> Tendo toda essa interface criada o setState poderá ser chamada para alterar o estado do objeto state que chamaráa a função ```build()``` novamente e vai reiniciar a árvore apartir no Build().
+
+ - didUpdateWidget() -> Quando há uma atualização em um widgetPai o widget filho chamará essa função para re-construir o seu estado. 
+
+- deactivate() -> Essa função é chamada quando o widget é removido da árvore de widgets porém não é destruído, um exemplo é a navegação entre página de que você pode voltar para a página inicial quando está em subpáginas. 
+
+ - dispose(): Este método é chamado quando o widget é removido permanentemente na árvore. 
+
+ ### Método build com uma melhor performace. 
+ https://www.youtube.com/watch?v=2gH8izRD-Io&list=PL_wKlpKIC9vWCRIgMvH8pbRmX8XVouRv1&index=7
--- lib/pages/CryptoPage.dart
+++ lib/pages/CryptoPage.dart
@@ -1,33 +1,113 @@
-
 import 'package:cryptomoedas/repositories/moeda_repository.dart';
+import 'package:cryptomoedas/models/moeda.dart'; 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; 
 
-
-class CryptoPage extends StatelessWidget 
+class CryptoPage extends StatefulWidget 
 {
   const CryptoPage({Key? key}) : super(key: key);
 
+  @override
+  State<CryptoPage> createState() => _CryptoPageState();
+}
+
+class _CryptoPageState extends State<CryptoPage> {
   @override 
+
+  
+  final moedas = MoedaRepository.moedas; 
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  
+  List<Moeda> selecionadas = []; 
+  
+  appBarDinamica(){
+
+    if(selecionadas.isEmpty){
+       return AppBar(
+                title: Text('Cripto Moedas'),
+              );
+    }
+    else {
+      return AppBar(
+        title: Text("${selecionadas.length} selecionadas", style: TextStyle(color: Colors.black),),
+        backgroundColor: Colors.blueGrey[50],
+        elevation: 1,
+        iconTheme: IconThemeData(color: Colors.black87),
+
+        leading: IconButton(icon: Icon(Icons.arrow_back),
+                       onPressed: (){
+                         setState(() {
+                           selecionadas = []; 
+                         });
+                       },
+        ),
+      ); 
+    }
+  }
+
   Widget build(BuildContext context )
   {
-
-    final moedas = MoedaRepository.moedas; 
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Crypto Moedas"),
-      ),
+      appBar: appBarDinamica(), 
       body: ListView.separated(itemBuilder: (BuildContext context, int moeda){
         return ListTile(
-          leading: Image.asset(moedas[moeda].icone),
-          title: Text(moedas[moeda].nome),
-          trailing: Text(moedas[moeda].preco.toString()),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
+          
+          leading: !(selecionadas.contains(moedas[moeda]))
+                   ?SizedBox(
+                      child: Image.asset(moedas[moeda].icone),
+                      width: 66,
+                      height: 66
+                    )
+                    : CircleAvatar(
+                     child: Icon(Icons.check),
+                    ),
+          title: Text(moedas[moeda].nome, 
+          style: TextStyle(
+            fontSize: 17, 
+            fontWeight: FontWeight.w500, 
+            color: Colors.black38
+           )
+          ),
+          trailing: Text(real.format(moedas[moeda].preco)),
+          selected: selecionadas.contains(moedas[moeda]),
+          selectedTileColor: Colors.indigo[50],
+          onLongPress: () {
+            setState(() {
+              selecionadas.contains(moedas[moeda]) 
+              ? selecionadas.remove(moedas[moeda])
+              : selecionadas.add(moedas[moeda]);
+              
+            });
+          },
+          onTap: (){
+            if(selecionadas.contains(moedas[moeda]))
+            {
+              setState(() {
+                selecionadas.remove(moedas[moeda]); 
+              });
+            }
+          },
         ); 
       }, 
                                padding: EdgeInsets.all(16),
                                separatorBuilder: (_,__) => Divider(), 
                                itemCount: moedas.length
                               )
+      ,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: selecionadas.isNotEmpty ? FloatingActionButton.extended(
+        onPressed: () { },
+        
+        icon: Icon(Icons.star),
+        label: Text("Favoritar", 
+         style: TextStyle(
+           letterSpacing: 0, 
+           fontWeight: FontWeight.bold
+         ),
+         )
+        ) 
+        : null,
     );
   }
 }
